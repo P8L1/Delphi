7e0f0a334253c7ff841408f326634f4c1bc454da
Pieter-Louis Schoeman <127837395+P8L1@users.noreply.github.com>
2025-04-15 14:30:18 +0200

    Update RandToDollar.dpr

--- RandToDollar.dpr
+++ RandToDollar.dpr
@@ -4,12 +4,12 @@ program RandToDollar;
 
 uses
   Forms,
-  Unit1 in 'Unit1.pas' {TForm1};
+  Unit1 in 'Unit1.pas' {TForm1, Form1};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, TForm1);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
