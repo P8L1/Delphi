3ad816a6ef4fb76718bf8364f0eb711649969a3d
pl1488 <pl.schoeman44@gmail.com>
2025-04-15 16:55:09 +0200

    More currencies

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,16 +1,16 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Rand to Dollar Converter'
-  ClientHeight = 200
-  ClientWidth = 400
+  Caption = 'Multi-Currency Converter'
+  ClientHeight = 250
+  ClientWidth = 500
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
-  OldCreateOrder = False
+  OldCreateOrder = True
   Position = poScreenCenter
   OnCreate = FormCreate
   OnDestroy = FormDestroy
@@ -18,32 +18,62 @@ object Form1: TForm1
   TextHeight = 13
   object lblPrompt: TLabel
     Left = 16
-    Top = 24
-    Width = 113
+    Top = 16
+    Width = 70
     Height = 13
-    Caption = 'Enter amount in Rands:'
+    Caption = 'Enter Amount:'
+  end
+  object lblSource: TLabel
+    Left = 16
+    Top = 48
+    Width = 84
+    Height = 13
+    Caption = 'Source Currency:'
+  end
+  object lblTarget: TLabel
+    Left = 240
+    Top = 48
+    Width = 83
+    Height = 13
+    Caption = 'Target Currency:'
   end
   object lblResult: TLabel
-    Left = 160
-    Top = 100
-    Width = 27
+    Left = 120
+    Top = 120
+    Width = 34
     Height = 13
-    Caption = 'USD: '
+    Caption = 'Result:'
   end
-  object edtRand: TEdit
-    Left = 160
-    Top = 20
-    Width = 200
+  object edtAmount: TEdit
+    Left = 120
+    Top = 12
+    Width = 100
     Height = 21
     TabOrder = 0
   end
+  object cbSource: TComboBox
+    Left = 120
+    Top = 44
+    Width = 100
+    Height = 21
+    Style = csDropDownList
+    TabOrder = 1
+  end
+  object cbTarget: TComboBox
+    Left = 340
+    Top = 44
+    Width = 100
+    Height = 21
+    Style = csDropDownList
+    TabOrder = 2
+  end
   object btnConvert: TButton
-    Left = 160
-    Top = 60
+    Left = 120
+    Top = 80
     Width = 75
     Height = 25
     Caption = 'Convert'
-    TabOrder = 1
+    TabOrder = 3
     OnClick = btnConvertClick
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -9,16 +9,19 @@ uses
 type
   TForm1 = class(TForm)
     lblPrompt: TLabel;
-    edtRand: TEdit;
+    edtAmount: TEdit;
+    lblSource: TLabel;
+    cbSource: TComboBox;
+    lblTarget: TLabel;
+    cbTarget: TComboBox;
     btnConvert: TButton;
     lblResult: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure btnConvertClick(Sender: TObject);
   private
-    { Private declarations }
+    procedure PopulateCurrencyList;
   public
-    { Public declarations }
   end;
 
 var
@@ -28,14 +31,14 @@ implementation
 
 {$R *.dfm}
 
-{ Helper: Return the JSON value for the specified key inside a TJSONObject }
+{ Helper function to get a JSON value from a TJSONObject by key }
 function GetJSONValue(JSONObj: TJSONObject; const Key: string): TJSONValue;
 var
   I: Integer;
   Pair: TJSONPair;
 begin
   Result := nil;
-  for I := 0 to JSONObj.size - 1 do
+  for I := 0 to JSONObj.Size - 1 do
   begin
     Pair := JSONObj.Get(I) as TJSONPair;
     if SameText(Pair.JsonString.Value, Key) then
@@ -46,42 +49,106 @@ begin
   end;
 end;
 
-procedure TForm1.FormCreate(Sender: TObject);
+{ Populate both source and target currency combo boxes with a fixed list.
+  For production, extend this list as needed to include all well-known currencies. }
+procedure TForm1.PopulateCurrencyList;
+var
+  i: Integer;
+  CurrencyList: array[0..9] of string;
 begin
-  { Initialize COM so we can use MSXML2.XMLHTTP }
-  CoInitialize(nil);
-  Assert(True, 'COM initialization assumed to succeed');
+  CurrencyList[0] := 'USD';
+  CurrencyList[1] := 'EUR';
+  CurrencyList[2] := 'GBP';
+  CurrencyList[3] := 'ZAR';
+  CurrencyList[4] := 'JPY';
+  CurrencyList[5] := 'AUD';
+  CurrencyList[6] := 'CAD';
+  CurrencyList[7] := 'CHF';
+  CurrencyList[8] := 'CNY';
+  CurrencyList[9] := 'INR';
+
+  cbSource.Items.BeginUpdate;
+  cbTarget.Items.BeginUpdate;
+  try
+    for i := Low(CurrencyList) to High(CurrencyList) do
+    begin
+      cbSource.Items.Add(CurrencyList[i]);
+      cbTarget.Items.Add(CurrencyList[i]);
+    end;
+    if cbSource.Items.IndexOf('ZAR') <> -1 then
+      cbSource.ItemIndex := cbSource.Items.IndexOf('ZAR')
+    else
+      cbSource.ItemIndex := 0;
+    if cbTarget.Items.IndexOf('USD') <> -1 then
+      cbTarget.ItemIndex := cbTarget.Items.IndexOf('USD')
+    else
+      cbTarget.ItemIndex := 0;
+  finally
+    cbSource.Items.EndUpdate;
+    cbTarget.Items.EndUpdate;
+  end;
+  Assert(cbSource.Items.Count > 0, 'Source currency list should not be empty');
+  Assert(cbTarget.Items.Count > 0, 'Target currency list should not be empty');
 end;
 
+{ Initialize COM and populate the currency lists }
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  CoInitialize(nil);
+  Assert(True, 'COM initialization assumed to succeed');
+  PopulateCurrencyList;
+end;
+
+{ Uninitialize COM on form destroy }
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
   CoUninitialize;
 end;
 
+{ On click of the Convert button, fetch conversion rate based on the chosen
+  source currency, extract the target rate from the JSON data, and show the result.
+  The function performs thorough error checking and assertions. }
 procedure TForm1.btnConvertClick(Sender: TObject);
 var
-  RandValue, DollarValue, RateValue: Double;
+  SourceAmount, ConvertedAmount, RateValue: Double;
   JSONResponse: string;
   XMLHTTP: OleVariant;
   JSONVal: TJSONValue;
-  JSONObj: TJSONObject;
-  RatesObj: TJSONObject;
-  USDRate: TJSONValue;
-  LBytes: TBytes;
+  JSONObj, RatesObj: TJSONObject;
+  TargetRate: TJSONValue;
+  URL: string;
+  Bytes: TBytes;
+  SourceCurrency, TargetCurrency: string;
 begin
-  { 1. Parse user input }
+  { 1. Parse user input amount }
   try
-    RandValue := StrToFloat(edtRand.Text);
+    SourceAmount := StrToFloat(edtAmount.Text);
   except
     on E: Exception do
     begin
-      ShowMessage('Invalid Rand value. Please enter a valid number.');
+      ShowMessage('Invalid amount. Please enter a valid number.');
       Exit;
     end;
   end;
-  Assert(RandValue >= 0, 'Input Rand value must be non-negative');
+  Assert(SourceAmount >= 0, 'Input amount must be non-negative');
 
-  { 2. Create MSXML2.XMLHTTP for the HTTPS GET call }
+  { 2. Retrieve selected source and target currencies }
+  if cbSource.ItemIndex < 0 then
+  begin
+    ShowMessage('Please select a source currency.');
+    Exit;
+  end;
+  if cbTarget.ItemIndex < 0 then
+  begin
+    ShowMessage('Please select a target currency.');
+    Exit;
+  end;
+  SourceCurrency := cbSource.Items[cbSource.ItemIndex];
+  TargetCurrency := cbTarget.Items[cbTarget.ItemIndex];
+  Assert(SourceCurrency <> '', 'Source currency must not be empty');
+  Assert(TargetCurrency <> '', 'Target currency must not be empty');
+
+  { 3. Create the MSXML2.XMLHTTP object for the HTTP GET call }
   try
     XMLHTTP := CreateOleObject('MSXML2.XMLHTTP');
   except
@@ -93,9 +160,13 @@ begin
   end;
   Assert(not VarIsClear(XMLHTTP), 'MSXML2.XMLHTTP object creation failed');
 
-  { 3. Fetch the JSON data from the API }
+  { 4. Build the API URL dynamically using the source currency }
+  URL := Format('https://api.exchangerate-api.com/v4/latest/%s', [SourceCurrency]);
+  Assert(URL <> '', 'Constructed API URL must not be empty');
+
+  { 5. Fetch JSON data from the API }
   try
-    XMLHTTP.open('GET', 'https://api.exchangerate-api.com/v4/latest/ZAR', False);
+    XMLHTTP.open('GET', URL, False);
     XMLHTTP.send('');
     JSONResponse := XMLHTTP.responseText;
   except
@@ -107,27 +178,52 @@ begin
   end;
   Assert(JSONResponse <> '', 'Received an empty API response');
 
-  { 4. Convert the JSON string to TBytes and parse }
-  LBytes := TEncoding.UTF8.GetBytes(JSONResponse);
-  JSONVal := TJSONObject.ParseJSONValue(LBytes, 0);
+  { 6. Parse JSON response }
+  Bytes := TEncoding.UTF8.GetBytes(JSONResponse);
+  JSONVal := TJSONObject.ParseJSONValue(Bytes, 0);
   Assert(JSONVal <> nil, 'Failed to parse JSON data');
-
-  { 5. Extract rate for USD from the JSON hierarchy }
   JSONObj := JSONVal as TJSONObject;
-  try
-    RatesObj := GetJSONValue(JSONObj, 'rates') as TJSONObject;
-    Assert(RatesObj <> nil, 'Rates object not found in JSON');
 
-    USDRate := GetJSONValue(RatesObj, 'USD');
-    Assert(USDRate <> nil, 'USD rate not found in rates data');
-
-    RateValue := StrToFloat(USDRate.Value);
-    DollarValue := RandValue * RateValue;
-    lblResult.Caption := Format('USD: %.2f', [DollarValue]);
-    Assert(DollarValue >= 0, 'Calculated USD value must be non-negative');
-  finally
-    JSONVal.Free;  // Also frees JSONObj
+  { 7. Extract the "rates" object from the JSON }
+  RatesObj := GetJSONValue(JSONObj, 'rates') as TJSONObject;
+  if RatesObj = nil then
+  begin
+    ShowMessage('Rates object not found in API response.');
+    JSONVal.Free;
+    Exit;
   end;
+  Assert(RatesObj <> nil, 'Rates object should not be nil');
+
+  { 8. Get the conversion rate for the target currency }
+  TargetRate := GetJSONValue(RatesObj, TargetCurrency);
+  if TargetRate = nil then
+  begin
+    ShowMessage(Format('Conversion rate for %s not found.', [TargetCurrency]));
+    JSONVal.Free;
+    Exit;
+  end;
+  Assert(TargetRate <> nil, 'Target rate must be found in the rates object');
+
+  try
+    RateValue := StrToFloat(TargetRate.Value);
+  except
+    on E: Exception do
+    begin
+      ShowMessage('Invalid conversion rate received from API.');
+      JSONVal.Free;
+      Exit;
+    end;
+  end;
+  Assert(RateValue > 0, 'Conversion rate must be greater than zero');
+
+  { 9. Compute the converted amount }
+  ConvertedAmount := SourceAmount * RateValue;
+  Assert(ConvertedAmount >= 0, 'Calculated converted amount must be non-negative');
+
+  { 10. Display result in label }
+  lblResult.Caption := Format('%s: %.2f', [TargetCurrency, ConvertedAmount]);
+
+  JSONVal.Free;
 end;
 
 end.
